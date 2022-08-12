1d9e5dead9661eb205a3ae171f2982558d808dca
KittyKittyKitKat <simon.palmer03@gmail.com>
2022-08-11 22:27:54 -0400

    Rewrote Chesstris after all the changes to the base game. Core gameplay loop established. Much more to come

--- Chesstris.py
+++ Chesstris.py
@@ -1,121 +1,151 @@
+from Tetris import Tetris, GoalType, PlacementType
+from Tetris import Square as TetrisSquare
 from Chess import Chess, Team
-from Tetris import Tetris
+from Chess import Square as ChessSquare
 
 import tkinter as tk
-from time import sleep
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from pygame import mixer
 
 class Chesstris:
-    def __init__(self, root_win, p1tetris, p2tetris, chess):
-        self.root = root_win
-        self.p1tetris = p1tetris
-        self.p2tetris = p2tetris
-        self.chess = chess
-        self.run_tetris = True
-        self.player_chess_move = None
+    def __init__(self, parent, chess, white_tetris, black_tetris):
+        self.parent = parent
+        self.chess: Chess = chess
+        self.white_tetris: Tetris = white_tetris
+        self.black_tetris: Tetris = black_tetris
+        self.parent_root = self.parent.winfo_toplevel()
+        self.active_game = Tetris
+        self.chess.pause_toggle()
+        self._set_up_game_event_hooks()
+        self.white_tetris.music_button.config(command=self.toggle_tetris_music)
+        self.black_tetris.music_button.config(command=self.toggle_tetris_music)
 
-    def create_games(self):
-        self.p1tetris.set_up_board()
-        # self.root.bind('s', lambda event: self.p1tetris.tetromino_fall())
-        self.root.bind('a', lambda event: self.p1tetris.tetromino_left())
-        self.root.bind('d', lambda event: self.p1tetris.tetromino_right())
-        self.root.bind('z', lambda event: self.p1tetris.tetromino_rotate('ccw'))
-        self.root.bind('x', lambda event: self.p1tetris.tetromino_rotate('cw'))
+        self.parent_root.bind('<Return>', lambda e: self.swap_games())
+        self.parent_root.bind('<space>', lambda e:(self.white_tetris.play_game(), self.black_tetris.play_game(), self.parent_root.unbind('<space>')))
 
-        self.p2tetris.set_up_board()
-        # self.root.bind('<Down>', lambda event: self.p2tetris.tetromino_fall())
-        self.root.bind('<Left>', lambda event: self.p2tetris.tetromino_left())
-        self.root.bind('<Right>', lambda event: self.p2tetris.tetromino_right())
-        self.root.bind('.', lambda event: self.p2tetris.tetromino_rotate('ccw'))
-        self.root.bind('/', lambda event: self.p2tetris.tetromino_rotate('cw'))
+    def _set_up_game_event_hooks(self):
+        self.chess.move_listener_flag = tk.StringVar(self.parent, value=self.chess.current_player.name)
+        self.move_listener_flag_id = self.chess.move_listener_flag.trace_add('write', self._chess_move_trace)
 
-        self.chess.set_up_board()
-        self.chess.create_classic_setup()
-        self.chess.locked = True
+        self.white_tetris.lines_cleared_flag = tk.IntVar(self.parent, value=-1)
+        self.white_lines_cleared_flag_id = self.white_tetris.lines_cleared_flag.trace_add('write', self._white_line_trace)
 
-    def spawn_tetris_pieces(self):
-        self.p1tetris.random_tetromino()
-        self.p2tetris.random_tetromino()
+        self.black_tetris.lines_cleared_flag = tk.IntVar(self.parent, value=-1)
+        self.black_lines_cleared_flag_id = self.black_tetris.lines_cleared_flag.trace_add('write', self._black_line_trace)
 
-    def lock_tetris(self):
-        self.run_tetris = False
-        self.root.unbind('s')
-        self.root.unbind('a')
-        self.root.unbind('d')
-        self.root.unbind('z')
-        self.root.unbind('x')
+    def _white_line_trace(self, *args):
+        # print(f'White cleared {self.white_tetris.lines_cleared_flag.get()} lines')
+        if self.white_tetris.lines_cleared_flag.get() > 0:
+            self.chess.change_player(override=Team.WHITE)
+            self.swap_games()
 
-        self.root.unbind('<Down>')
-        self.root.unbind('<Left>')
-        self.root.unbind('<Right>')
-        self.root.unbind('.')
-        self.root.unbind('/')
+    def _black_line_trace(self, *args):
+        # print(f'Black cleared {self.black_tetris.lines_cleared_flag.get()} lines')
+        if self.black_tetris.lines_cleared_flag.get() > 0:
+            self.chess.change_player(override=Team.BLACK)
+            self.swap_games()
 
-    def unlock_tetris(self):
-        self.run_tetris = True
-        # self.root.bind('s', lambda event: self.p1tetris.tetromino_fall())
-        self.root.bind('a', lambda event: self.p1tetris.tetromino_left())
-        self.root.bind('d', lambda event: self.p1tetris.tetromino_right())
-        self.root.bind('z', lambda event: self.p1tetris.tetromino_rotate('ccw'))
-        self.root.bind('x', lambda event: self.p1tetris.tetromino_rotate('cw'))
+    def _chess_move_trace(self, *args):
+        self.swap_games()
 
-        # self.root.bind('<Down>', lambda event: self.p2tetris.tetromino_fall())
-        self.root.bind('<Left>', lambda event: self.p2tetris.tetromino_left())
-        self.root.bind('<Right>', lambda event: self.p2tetris.tetromino_right())
-        self.root.bind('.', lambda event: self.p2tetris.tetromino_rotate('ccw'))
-        self.root.bind('/', lambda event: self.p2tetris.tetromino_rotate('cw'))
+    def toggle_tetris_music(self):
+        if self.white_tetris.music_channel.get_volume() == 0:
+            self.white_tetris.music_button.config(image=self.white_tetris.texts['\U0001D195'])
+            self.black_tetris.music_button.config(image=self.black_tetris.texts['\U0001D195'])
+            self.white_tetris.music_channel.set_volume(0.1)
+        else:
+            self.white_tetris.music_channel.set_volume(0)
+            self.white_tetris.music_button.config(image=self.white_tetris.texts['\U0001D194'])
+            self.black_tetris.music_button.config(image=self.black_tetris.texts['\U0001D194'])
 
-    def play_chess(self):
-        self.chess.locked = False
-        self.chess.current_player = self.player_chess_move
+    def swap_games(self):
+        self.white_tetris.pause_game()
+        self.black_tetris.pause_game()
+        self.chess.pause_toggle()
 
 
 if __name__ == '__main__':
+    mixer.pre_init(buffer=4096)
+    mixer.init()
+    tetris_music = mixer.Channel(0)
+    tetris_w_move = mixer.Channel(2)
+    tetris_w_line = mixer.Channel(3)
+    # tetris_b_music = mixer.Channel(4)
+    tetris_b_move = mixer.Channel(5)
+    tetris_b_line = mixer.Channel(6)
+    chess_sound = mixer.Channel(7)
+
     root = tk.Tk()
     root.resizable(0, 0)
     root.title('Chesstris')
-    root.config(bg='#333')
-    p1frame = tk.Frame(root, height=Tetris.SQUARE_SIZE*Tetris.ROWS, width=Tetris.SQUARE_SIZE*Tetris.COLUMNS)
-    p2frame = tk.Frame(root, height=Tetris.SQUARE_SIZE*Tetris.ROWS, width=Tetris.SQUARE_SIZE*Tetris.COLUMNS)
-    chessframe = tk.Frame(root, height=Chess.SQUARE_SIZE*Chess.RANKS, width=Chess.SQUARE_SIZE*Chess.FILES)
-    p1frame.grid_propagate(False)
-    p2frame.grid_propagate(False)
-    chessframe.grid_propagate(False)
-    player_1_tetris = Tetris(p1frame)
-    player_2_tetris = Tetris(p2frame)
-    chess = Chess(chessframe)
-    p1frame.grid(row=0, column=0)
-    p2frame.grid(row=0, column=2)
-    chessframe.grid(row=0, column=1)
-    chesstris = Chesstris(root, player_1_tetris, player_2_tetris, chess)
-    chesstris.create_games()
-    game_time = 0
-    while True:
-        try:
-            root.update_idletasks()
-            root.update()
-            sleep(0.01)
-            game_time += 1
-            if not chesstris.chess.locked:
-                if chesstris.chess.move_made == 'Success':
-                    chesstris.chess.locked = True
-                    chesstris.chess.move_made = None
-                    chesstris.unlock_tetris()
-            if game_time == 25:
-                game_time = 0
-                if chesstris.run_tetris:
-                    chesstris.spawn_tetris_pieces()
-                    p1_line_cleared = chesstris.p1tetris.tetromino_fall()
-                    p2_line_cleared = chesstris.p2tetris.tetromino_fall()
-                    if chesstris.p1tetris.has_lost or chesstris.p2tetris.has_lost:
-                        break
-                    if p1_line_cleared:
-                        chesstris.player_chess_move = Team.WHITE
-                        chesstris.lock_tetris()
-                        chesstris.play_chess()
-                    if p2_line_cleared:
-                        chesstris.player_chess_move = Team.BLACK
-                        chesstris.lock_tetris()
-                        chesstris.play_chess()
-        except tk.TclError:
-            break
+    root.config(bg='black')
 
+    tetris_w_frame = tk.Frame(root)
+    tetris_b_frame = tk.Frame(root)
+    chess_frame = tk.Frame(root)
+    tetris_w_frame.grid_propagate(False)
+    tetris_b_frame.grid_propagate(False)
+    chess_frame.grid_propagate(False)
+
+    tetris_w_keys = {
+        'soft drop': 's',
+        'hard drop': 'w',
+        'left': 'a',
+        'right': 'd',
+        'rotate clockwise': 'x',
+        'rotate counterclockwise': 'z',
+        'hold': 'c'
+    }
+    tetris_b_keys = {
+        'soft drop': 'Down',
+        'hard drop': 'Up',
+        'left': 'Left',
+        'right': 'Right',
+        'rotate clockwise': 'b',
+        'rotate counterclockwise': 'n',
+        'hold': 'm'
+    }
+
+    tetris_white = Tetris(
+        parent=tetris_w_frame,
+        mirror_ui=True,
+        ghost_piece = True,
+        placement_mode=PlacementType.EXTENDED,
+        starting_level=5,
+        goal_type=GoalType.STAGNANT,
+        key_mapping=tetris_w_keys,
+        allow_pausing=False,
+        music_channel=tetris_music,
+        move_channel=tetris_w_move,
+        line_channel=tetris_w_line,
+        start_menu=False,
+        allow_play_again=False
+    )
+    tetris_black = Tetris(
+        parent=tetris_b_frame,
+        mirror_ui=False,
+        ghost_piece = True,
+        placement_mode=PlacementType.EXTENDED,
+        starting_level=5,
+        goal_type=GoalType.STAGNANT,
+        key_mapping=tetris_b_keys,
+        allow_pausing=False,
+        music_channel=tetris_music,
+        move_channel=tetris_b_move,
+        line_channel=tetris_b_line,
+        start_menu=False,
+        allow_play_again=False
+    )
+    chess = Chess(
+        parent=chess_frame,
+        square_sheet='assets/chess/squares.png',
+        sound_channel=chess_sound,
+        flip_after_move=False,
+        allow_play_again=False
+    )
+    chesstris = Chesstris(root, chess, tetris_white, tetris_black)
+    tetris_w_frame.grid(row=0, column=0)
+    tetris_b_frame.grid(row=0, column=2)
+    chess_frame.grid(row=0, column=1)
+    root.mainloop()
